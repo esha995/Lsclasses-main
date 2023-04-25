ee8a5749c54bdaada1d6baa4ea984286e6d4be9a
Vishal Tiwari <69195646+vishal3113@users.noreply.github.com>
2023-04-25 21:18:05 +0530

    Delete demo-classic/Backend directory

--- demo-classic/Backend/functions_fetch_st2.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../partial/db_connect.php';
-// include "../Sessions.php";
-    $sql="SELECT `overall_attendance` FROM `attendance` WHERE Rollno='$_SESSION[Rollno]'";
-    $result=mysqli_query($connect_db_ls,$sql);
-    while($row=$result->fetch_assoc()){
-        $_SESSION['attendance']=$row['overall_attendance'];
-    }
-?>
